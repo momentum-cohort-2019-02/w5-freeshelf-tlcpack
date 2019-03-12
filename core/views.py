b5e5c1d44a4b0c1c0445f1323a2c0eef5d9d239a
Taylor Cooke <taylorcooke@gmail.com>
2019-03-12 14:17:02 -0400

    index page displays

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from core.models import Book, Category
 
 # Create your views here.
+def index(request):
+    """View function home page of site"""
+
+    # in case we want to count stuff
+    num_books = Book.objects.all().count()
+    num_categories = Category.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_categories': num_categories,
+    }
+
+    return render(request, 'index.html', context=context)

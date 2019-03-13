e9c9130a3a950632137e19f2483363076cb9bcc6
Taylor Cooke <taylorcooke@gmail.com>
2019-03-12 21:31:32 -0400

    added list and detail pages. but still not getting correct pages to display

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from core.models import Book, Category
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -15,3 +16,17 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class CategoryListView(generic.ListView):
+    model = Category
+    paginate_by = 5
+
+class CategoryDetailView(generic.DetailView):
+    model = Category
